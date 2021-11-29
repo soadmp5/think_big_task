0706dac9209d6bbdb188e8e9f77862f563bc219c
Phreak <batteryoflife@gmail.com>
2021-11-29 15:16:24 +0200

    removed couple mistakes from SourceDetails.js

--- src/SourceDetails.js
+++ src/SourceDetails.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import "./App.css";
 
-function SourceDetails(props) {
+function SourceDetails() {
   useEffect(() => {
     fetchItem();
   }, []);
--- src/SourcePages.js
+++ src/SourcePages.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import "./App.css";
 import { Link } from "react-router-dom";
 
-function SourcePages(props) {
+function SourcePages() {
   useEffect(() => {
     fetchItems();
   }, []);
